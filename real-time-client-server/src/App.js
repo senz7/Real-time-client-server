cd68808c15d310fda4fcbab7ea55b18b9fa5e1f1
Arseniy <viplol1279@mail.ru>
2024-03-09 22:48:12 +0300

    init

---
+++ real-time-client-server/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import React from "react";
+import Websocket from "./Websocket";
+
+function App() {
+  return (
+    <div className="App">
+      <Websocket />
+    </div>
+  );
+}
+
+export default App;
